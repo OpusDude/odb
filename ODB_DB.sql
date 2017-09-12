5e976c5a4a60bb262111815af11323a4d0b113e3
Steve Weber <steveweb@microsoft.com>
2017-09-12 17:59:13 -0400

    Updated DB

--- ODB_DB.sql
+++ ODB_DB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 05, 2017 at 06:57 PM
+-- Generation Time: Sep 12, 2017 at 05:57 PM
 -- Server version: 5.7.19-0ubuntu0.16.04.1
 -- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
@@ -28,13 +28,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Donation` (
   `Id` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Vendor` varchar(50) NOT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  `Driver` varchar(50) DEFAULT NULL,
+  `Vendor` int(11) NOT NULL,
+  `Driver` int(11) DEFAULT NULL,
   `Items` varchar(250) NOT NULL,
   `ItemDesc` varchar(50) NOT NULL,
   `Quantity` int(11) DEFAULT NULL,
+  `QuantityType` varchar(10) NOT NULL,
   `Value` decimal(10,0) DEFAULT NULL,
   `Weight` int(11) DEFAULT NULL,
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -44,9 +43,9 @@ CREATE TABLE `Donation` (
 -- Dumping data for table `Donation`
 --
 
-INSERT INTO `Donation` (`Id`, `Name`, `Vendor`, `Email`, `Driver`, `Items`, `ItemDesc`, `Quantity`, `Value`, `Weight`, `Date`) VALUES
-(1, 'John Doe', 'Kroger', 'johndoe@kroger.com', 'Pete Spade', 'Bread', '', 15, '20', 4, '2017-08-08 16:08:27'),
-(2, 'Betty Rubble', 'Avrils Deli Meats', 'betty@myhouse.com', 'Ted Ginn', 'Meat', 'Hotdogs', 50, '70', 15, '2017-09-01 11:53:48');
+INSERT INTO `Donation` (`Id`, `Vendor`, `Driver`, `Items`, `ItemDesc`, `Quantity`, `QuantityType`, `Value`, `Weight`, `Date`) VALUES
+(3, 4, 2, 'Produce', 'Potatos', 34, 'Bags', '12', 3, '2017-09-11 17:18:33'),
+(4, 1, 1, 'Fruit', 'Apples', 15, 'Crates', '25', 13, '2017-09-11 17:34:16');
 
 -- --------------------------------------------------------
 
@@ -73,7 +72,8 @@ CREATE TABLE `Vendor` (
 INSERT INTO `Vendor` (`Id`, `Vendor`, `Contact`, `Email`, `Address`, `City`, `State`, `ZipCode`, `PhoneNumber`) VALUES
 (1, 'Freds Produce', 'Fred Johnson', 'fred@produce.us', '967 Orange St', 'Cincinnati', 'OH', '45212', '513-934-1189'),
 (2, 'Tinas Cookies', 'Tina Lacky', 'Tina@cookies.com', '1334 Butter Ave', 'Cincinnati', 'OH', '45212', ''),
-(4, 'Joes Potatos', 'Joe Spud', 'joe@spuds.com', '222 Sweet Potato Dr', 'Cincinnati', 'OH', '45321', '513-944-4456');
+(4, 'Joes Potatos', 'Joe Spud', 'joe@spuds.com', '222 Sweet Potato Dr', 'Cincinnati', 'OH', '45321', '513-944-4456'),
+(5, 'Avrils Deli Meats', 'John Baker', 'johnb@avrilsmeats.com', '1234 Biltmore St', 'Cincinnati', 'OH', '45233', '513-123-4555');
 
 --
 -- Indexes for dumped tables
@@ -101,12 +101,12 @@ ALTER TABLE `Vendor`
 -- AUTO_INCREMENT for table `Donation`
 --
 ALTER TABLE `Donation`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `Vendor`
 --
 ALTER TABLE `Vendor`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
